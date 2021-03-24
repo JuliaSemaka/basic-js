87c05093ba05f2642945da18b8836ec4c0bf262b
JuliaSemaka <ysemaka@mail.ru>
2021-03-25 01:40:14 +0300

    feat: implement five functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !Array.isArray(members)) {
+    return false;
+  }
+  let newArr = members.filter(function (item) {
+    return typeof item === "string";
+  });
+  let firstLetters = newArr.map(function (item) {
+    return item.trim().slice(0, 1).toUpperCase();
+  });
+  return firstLetters.sort().join("");
 };

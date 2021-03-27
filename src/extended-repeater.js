276f6808ca3a4b7ce4f77b9af9fe852252f4a644
JuliaSemaka <ysemaka@mail.ru>
2021-03-27 21:39:42 +0300

    feat: incomming extended-repeter function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (typeof str !== "string") {
+    str = str + "";
+  }
+  if (options.addition === undefined) {
+    options.addition = "";
+  }
+  if (typeof options.addition !== "string") {
+    options.addition = options.addition + "";
+  }
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.separator) {
+    options.separator = "+";
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+
+  let resAddition = "";
+  let res = "";
+  for (let i = 1; i < options.additionRepeatTimes; i++) {
+    resAddition += options.addition + options.additionSeparator;
+  }
+  resAddition += options.addition;
+
+  for (let i = 1; i < options.repeatTimes; i++) {
+    res += str + resAddition + options.separator;
+  }
+  res += str + resAddition;
+  return res;
 };
-  
